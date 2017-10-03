0aeabcbf246ff4bdc2af78f965b8c13a5e4252b7
Darko Kukovec <darko@kukovec.eu>
2017-10-03 17:59:20 +0200

    Configure webpack canary

---
+++ webpack-canary.conf.js
@@ -0,0 +1,10 @@
+module.exports = {
+  versions: {
+    'v3.6.0': [{
+      dependency: 'https://github.com/webpack-contrib/extract-text-webpack-plugin/archive/master.tar.gz',
+      exampleDir: 'example',
+      test: 'npm test',
+    }],
+  },
+  loglevel: 'trace',
+};
